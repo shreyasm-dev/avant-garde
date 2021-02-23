6979ace0e0f33ad5b661636e8316de8e311fa06e
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2021-02-23 16:30:51 -0600

    Added progress bar function

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ const console = {
     process.stdout.write(`${content.join(' ')}\n`);
     return content.join(' '); // Functions return the value they are logging
   },
+
+  /**
+   * Print a progress bar
+   * @param {int} [percentage=100] - Progress bar percentage
+   * @param {string} [character='\u2588'] - Character used in progress bar
+   * @param {int} [columns=process.stdout.columns]
+   */
+
+  progress: (percentage = 100, character = '\u2588', columns = process.stdout.columns) => {
+    process.stdout.write(`${character.repeat(columns * (percentage / 100))}\r`);
+    return character.repeat(columns * (percentage / 100));
+  },
 };
 
 export default console;
