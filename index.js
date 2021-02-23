9387d902659ce0c31e30965e68dfa1a8f884b13c
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2021-02-23 15:24:06 -0600

    Added log function

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 import 'core-js';
 
-const console = {};
+const console = {
+
+  /**
+   * Log a string to the console
+   * @param  {...string} content - The content to log to the console
+   * @returns {string}
+   */
+
+  log: (...content) => {
+    process.stdout.write(`${content.join(' ')}\n`);
+    return content.join(' '); // Functions return the value they are logging
+  },
+};
 
 export default console;
