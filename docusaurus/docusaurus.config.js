b868e738a7ded8e775730ac7c9d76f43881f656a
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2021-02-23 18:30:35 -0600

    Changed docusaurus config

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  title: 'Avant Garde',
+  tagline: 'A newer, better, console',
+  url: 'https://shreyasm-dev.github.io',
+  baseUrl: '/avant-garde',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'shreyasm-dev', // Usually your GitHub org/user name.
+  projectName: 'avante-garde', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'Avant Garde',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Avant Garde Logo',
         src: 'img/logo.svg',
       },
       items: [
@@ -22,9 +22,8 @@ module.exports = {
           label: 'Docs',
           position: 'left',
         },
-        {to: 'blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/shreyasm-dev/avant-garde',
           label: 'GitHub',
           position: 'right',
         },
@@ -34,50 +33,20 @@ module.exports = {
       style: 'dark',
       links: [
         {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Style Guide',
-              to: 'docs/',
-            },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2/',
-            },
-          ],
-        },
-        {
           title: 'Community',
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/shreyasm-dev/avant-garde',
+            },
+            {
+              label: 'NPM',
+              href: 'https://npmjs.com/package/avant-garde',
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} shreyasm-dev. Built with Docusaurus.`,
     },
   },
   presets: [
@@ -88,13 +57,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/shreyasm-dev/avant-garde/edit/master/docusaurus/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
