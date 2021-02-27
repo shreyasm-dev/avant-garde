3a8a11615478b826b5d26df902f0ab1602fd287b
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2021-02-27 11:33:43 -0600

    Improved homepage

--- docusaurus/src/pages/index.js
+++ docusaurus/src/pages/index.js
@@ -8,32 +8,29 @@ import styles from './styles.module.css';
 
 const features = [
   {
-    title: 'Easy to Use',
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    title: 'Well Maintained',
+    imageUrl: 'img/undraw_clean_up.svg',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        This project has almost-daily commits and regular bug-fixes
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
+    title: 'Easy to Contribute to',
+    imageUrl: 'img/undraw_contribute.svg',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        We have JS style guidelines and contributing guidelines that make it easy to code
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    imageUrl: 'img/undraw_docusaurus_react.svg',
+    title: 'Friendly Community',
+    imageUrl: 'img/undraw_community.svg',
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        You can reach out to us at [GitHub](https://github.com/shreyasm-dev/avant-garde) for help
       </>
     ),
   },
