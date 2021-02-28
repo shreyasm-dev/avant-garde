31e37484d8bdc4197b77897d7f534950d7dc3712
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2021-02-28 13:04:33 -0600

    Wrote tests for console.animation

---
+++ test/console.animation.js
@@ -0,0 +1,18 @@
+import { assert } from 'chai';
+import console from '../index.js'; // eslint-disable-line import/extensions
+
+describe('console#animation', () => {
+  it('should play an animation', () => {
+    const frames = ['-', '--', '---'];
+    const printedFrames = [];
+    console.animation(frames, 0, (iteration, frame) => {
+      printedFrames.push(frame);
+
+      if (iteration === 2) { // Last frame
+        assert.deepEqual(frames, printedFrames);
+      }
+
+      return iteration !== 2;
+    }, true);
+  });
+});
