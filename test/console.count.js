5cff9c7038f4f2229fb3bd37326b72544b6f9a09
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2021-03-02 18:09:34 -0600

    Fixed issue where console#count doesn't show up when tests are run

--- test/console.count.js
+++ test/console.count.js
@@ -2,7 +2,7 @@ import { assert } from 'chai';
 import mute from 'mute';
 import console from '../index.js'; // eslint-disable-line import/extensions
 
-describe('console#count', () => {
+describe('console#count', async () => {
   it('should count a label', () => {
     const unmute = mute();
 
