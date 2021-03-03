cbfeecdbc17ab636d2c07752bcdb562d5cb0fe47
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2021-03-02 21:28:55 -0600

    Wrote tests for console.debug

---
+++ test/console.debug.js
@@ -0,0 +1,15 @@
+import { assert } from 'chai';
+import hookStd from 'hook-std';
+import console from '../index.js'; // eslint-disable-line import/extensions
+
+describe('console#debug', () => {
+  it('should print blue text', async () => {
+    const promise = hookStd.stdout((output, unhook) => {
+      unhook();
+      assert.strictEqual(output, '\u001b[36mHello, World!\u001b[36m\n');
+    });
+
+    console.debug('Hello,', 'World!');
+    await promise;
+  });
+});
